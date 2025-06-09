043715fbf4e6718dcfa27dd0adfd709acc0b5c7d
Omar <ovivasar@gmail.com>
2025-06-09 10:16:49 -0500

    firmax

--- src/controllers/cpesunat.controllers.js
+++ src/controllers/cpesunat.controllers.js
@@ -66,6 +66,9 @@ const registrarCPESunat = async (req,res,next)=> {
 
 
 async function firmarXMLUBL(unsignedXML, ruc) {
+  verificarAPIXAdES();
+  
+  // Consulta certificado y password
   const { rows } = await pool.query(`
     SELECT certificado, password
     FROM mad_usuariocertificado 
@@ -76,23 +79,26 @@ async function firmarXMLUBL(unsignedXML, ruc) {
 
   const { certificado: certificadoBuffer, password } = rows[0];
 
+  // Carga PFX desde buffer y obtiene clave y certificado
   const p12Asn1 = forge.asn1.fromDer(forge.util.createBuffer(certificadoBuffer));
   const p12 = forge.pkcs12.pkcs12FromAsn1(p12Asn1, false, password);
 
   const privateKey = p12.getBags({ bagType: forge.pki.oids.pkcs8ShroudedKeyBag })[forge.pki.oids.pkcs8ShroudedKeyBag][0].key;
-  const certPEM = forge.pki.certificateToPem(
-    p12.getBags({ bagType: forge.pki.oids.certBag })[forge.pki.oids.certBag][0].cert
-  );
+  const certForge = p12.getBags({ bagType: forge.pki.oids.certBag })[forge.pki.oids.certBag][0].cert;
+  const certPEM = forge.pki.certificateToPem(certForge);
 
+  // Parsear XML sin firmar
   const doc = new DOMParser().parseFromString(unsignedXML, 'text/xml');
 
+  // Limpiar contenido de UBLExtensions
   const select = xpath.useNamespaces({
     ext: 'urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2'
   });
-
   const ublExtensions = select('//ext:UBLExtensions', doc)[0];
-  if (ublExtensions) while (ublExtensions.firstChild) ublExtensions.removeChild(ublExtensions.firstChild);
+  if (!ublExtensions) throw new Error('No se encontró el nodo UBLExtensions');
+  while (ublExtensions.firstChild) ublExtensions.removeChild(ublExtensions.firstChild);
 
+  // Convertir clave privada y cargar en crypto.subtle
   const privateKeyBuffer = convertPrivateKeyToPkcs8Buffer(privateKey);
   const privateKeyCrypto = await xadesjs.Application.crypto.subtle.importKey(
     "pkcs8",
@@ -102,63 +108,76 @@ async function firmarXMLUBL(unsignedXML, ruc) {
     ["sign"]
   );
 
+  // Crear objeto SignedXml
   const xmlSig = new xadesjs.SignedXml();
   xmlSig.SigningKey = privateKeyCrypto;
 
-  const reference = new xadesjs.Reference();
-  reference.Uri = "";
-  reference.DigestMethod = { Algorithm: "http://www.w3.org/2001/04/xmlenc#sha256" };
-  reference.AddTransform(new xadesjs.Transforms.Enveloped());
-  reference.AddTransform(new xadesjs.Transforms.C14N());
-
-  xmlSig.SignedInfo.References.push(reference);
-
+  // Firmar XML pasando referencias directo en options
   await xmlSig.Sign(
     { name: "RSASSA-PKCS1-v1_5" },
-    privateKeyCrypto,
-    doc.documentElement
+    doc.documentElement,
+    {
+      references: [
+        {
+          hash: "SHA-256",
+          transforms: [
+            "http://www.w3.org/2000/09/xmldsig#enveloped-signature",
+            "http://www.w3.org/TR/2001/REC-xml-c14n-20010315"
+          ]
+        }
+      ]
+    }
   );
 
-  const signatureElement = xmlSig.XmlSignature;
-  if (!signatureElement) throw new Error('No se pudo obtener el elemento signature');
+  // Obtener elemento Signature generado
+  const signatureElement = xmlSig.XmlSignature || (typeof xmlSig.GetXml === 'function' && xmlSig.GetXml());
+  if (!signatureElement) throw new Error('No se pudo obtener el elemento Signature');
 
+  // Agregar certificado manualmente al KeyInfo
   const rawCert = Buffer.from(certPEM.replace(/(-----(BEGIN|END) CERTIFICATE-----|\n)/g, ""), 'base64');
 
-  const keyInfo = doc.createElementNS('http://www.w3.org/2000/09/xmldsig#', 'KeyInfo');
+  const keyInfo = signatureElement.getElementsByTagNameNS('http://www.w3.org/2000/09/xmldsig#', 'KeyInfo')[0]
+    || doc.createElementNS('http://www.w3.org/2000/09/xmldsig#', 'KeyInfo');
+
   const x509Data = doc.createElementNS('http://www.w3.org/2000/09/xmldsig#', 'X509Data');
   const x509Certificate = doc.createElementNS('http://www.w3.org/2000/09/xmldsig#', 'X509Certificate');
   x509Certificate.appendChild(doc.createTextNode(rawCert.toString('base64')));
   x509Data.appendChild(x509Certificate);
   keyInfo.appendChild(x509Data);
-  signatureElement.appendChild(keyInfo);
 
+  // En caso que KeyInfo no esté dentro de Signature, agregarlo
+  if (!signatureElement.contains(keyInfo)) {
+    signatureElement.appendChild(keyInfo);
+  }
+
+  // Insertar Signature dentro de UBLExtensions > UBLExtension > ExtensionContent
   const ublExtension = doc.createElementNS('urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2', 'ext:UBLExtension');
   const extensionContent = doc.createElementNS('urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2', 'ext:ExtensionContent');
   const importedSignature = doc.importNode(signatureElement, true);
+
   extensionContent.appendChild(importedSignature);
   ublExtension.appendChild(extensionContent);
   ublExtensions.appendChild(ublExtension);
 
+  // Devolver XML firmado serializado
   return new XMLSerializer().serializeToString(doc);
 }
 
+
 function verificarAPIXAdES() {
-  console.log('Verificando API XAdES.js:');
-  if (typeof xadesjs.SignedXml === 'function') {
-    const testInstance = new xadesjs.SignedXml();
-    console.log('Métodos disponibles:', {
-      Sign: typeof testInstance.Sign,
-      ComputeSignature: typeof testInstance.ComputeSignature,
-      AddReference: typeof testInstance.AddReference
+  console.log('Verificando API de XAdES.js disponible:');
+  console.log('xadesjs:', typeof xadesjs);
+  console.log('xadesjs.xml:', typeof xadesjs.xml);
+  console.log('xadesjs.xml.Reference:', typeof xadesjs.xml?.Reference);
+  console.log('xadesjs.SignedXml:', typeof xadesjs.SignedXml);
+  
+  // Verificar qué constructores están disponibles
+  if (xadesjs.xml) {
+    console.log('Constructores disponibles en xadesjs.xml:');
+    Object.keys(xadesjs.xml).forEach(key => {
+      console.log(`- ${key}:`, typeof xadesjs.xml[key]);
     });
   }
-}
-
-function convertPrivateKeyToPkcs8Buffer(privateKey) {
-  const privateKeyAsn1 = forge.pki.privateKeyToAsn1(privateKey);
-  const privateKeyInfoAsn1 = forge.pki.wrapRsaPrivateKey(privateKeyAsn1);
-  const derBuffer = forge.asn1.toDer(privateKeyInfoAsn1).getBytes();
-  return new Uint8Array([...derBuffer].map(c => c.charCodeAt(0)));
 }
 
 //////////////////////////////////////////////////////////////////////////////

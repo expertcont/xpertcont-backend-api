0c2b9bdb2602b6578a3d389f204ea56bed2345f6
Omar <ovivasar@gmail.com>
2025-06-18 12:01:15 -0500

    pdfx

--- src/controllers/cpe/cpegenerapdf.js
+++ src/controllers/cpe/cpegenerapdf.js
@@ -263,4 +263,14 @@ const cpegenerapdf = async (size, logo, jsonVenta) => {
   
 }
 
+  function base64ToUint8Array(base64) {
+    const binaryString = window.atob(base64); // Decodificar Base64 a binario
+    const len = binaryString.length;
+    const bytes = new Uint8Array(len);
+    for (let i = 0; i < len; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes;
+  }
+
 module.exports = cpegenerapdf;

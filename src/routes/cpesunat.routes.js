0c3c89caf3f1d79bb81a235c5533e95d49ad2789
Omar <ovivasar@gmail.com>
2025-11-24 03:38:58 -0500

    reproceso cdr

--- src/routes/cpesunat.routes.js
+++ src/routes/cpesunat.routes.js
@@ -1,14 +1,14 @@
 const {Router} = require('express');
 const router = Router();
 const {registrarCPESunat, registrarCPESunatPrevioPDF, registrarCPESunatPrevioPDFA4} = require('../controllers/cpesunat.controllers')
-const {consultarCDRSunat} = require('../controllers/cpesunatgetcdr.controllers')
+const {procesarCDRPendienteSunat} = require('../controllers/cpesunatgetcdr.controllers')
 //router.get('/cpesunat', obtenerTodosUsuarios);
 
 router.post('/cpesunat', registrarCPESunat);
 router.post('/cpesunatpdfprevio', registrarCPESunatPrevioPDF);
 router.post('/cpesunatpdfprevioa4', registrarCPESunatPrevioPDFA4);
 
-router.post('/cpesunatgetcdr', consultarCDRSunat);
+router.post('/cpesunatgetcdr', procesarCDRPendienteSunat); //en caso se use independiente
 //router.put('/usuario/:id_usuario', actualizarUsuario);
 //router.delete('/usuario/:id_usuario', eliminarUsuario);
 

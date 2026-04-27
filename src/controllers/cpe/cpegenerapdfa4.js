1647c11b92f7a01d890da28d42071e8bd6bbebdc
unknown <ovivasar@gmail.com>
2026-04-27 12:38:04 -0500

    mod formapago pdf

--- src/controllers/cpe/cpegenerapdfa4.js
+++ src/controllers/cpe/cpegenerapdfa4.js
@@ -116,7 +116,11 @@ function drawHeader(page, { pngImage, pngDims, empresa, venta, cliente, font, fo
   if (venta.vendedor?.trim()) {
     page.drawText('Vendedor: '+venta.vendedor.trim(), { x:MARGIN_L+5, y, size:9, font }); y -= 11;
   }
-  page.drawText('Forma de Pago: CONTADO', { x:MARGIN_L+5, y, size:9, font }); y -= 10;
+  page.drawText('Forma de Pago: CONTADO', { x:MARGIN_L+5, y, size:9, font }); 
+  page.drawText((venta.forma_pago2 || ''), { x:MARGIN_L+25, y, size:9, font });
+  page.drawText((venta.efectivo2 || ''), { x:MARGIN_L+45, y, size:9, font });
+  y -= 10;
+  
 
   return y;
 }

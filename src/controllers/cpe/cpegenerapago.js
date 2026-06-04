25e3508d8ecd753b6fe464da03106a3e63724efa
unknown <ovivasar@gmail.com>
2026-06-04 00:25:00 -0500

    formapago moneda

--- src/controllers/cpe/cpegenerapago.js
+++ src/controllers/cpe/cpegenerapago.js
@@ -1,10 +1,41 @@
 function cpegenerapago(data) {
+  //Considerar 1 sola cuota, por el momento
+  const toNumber = (val) => Number(val) || 0;
+  let subtotal = toNumber(data.base_gravada) + toNumber(data.base_exonerada) + toNumber(data.base_inafecta) + toNumber(data.base_gratuita);
+  let igv = toNumber(data.total_igv);
+  let nPrecioTotal = (subtotal + igv).toFixed(2);
+
+  if (data.forma_pago_id === 'Contado') {
+    return `
+    <cac:PaymentTerms>
+      <cbc:ID>FormaPago</cbc:ID>
+      <cbc:PaymentMeansID>Contado</cbc:PaymentMeansID>
+    </cac:PaymentTerms>`;
+  };
+
+  return `
+  <cac:PaymentTerms>
+    <cbc:ID>FormaPago</cbc:ID>
+    <cbc:PaymentMeansID>Credito</cbc:PaymentMeansID>
+  </cac:PaymentTerms>
+  <cac:PaymentTerms>
+    <cbc:ID>Cuota001</cbc:ID>
+    <cbc:Amount currencyID="${data.moneda_id}">
+      ${nPrecioTotal}
+    </cbc:Amount>
+    <cbc:PaymentDueDate>
+      ${data.fecha_vencimiento}
+    </cbc:PaymentDueDate>
+  </cac:PaymentTerms>`;
+}
+
+/*function cpegenerapago(data) {
   //falta especificar arreglo de cuotas de pago
   return `
   <cac:PaymentTerms>
     <cbc:ID>FormaPago</cbc:ID>
     <cbc:PaymentMeansID>${data.forma_pago_id}</cbc:PaymentMeansID>
   </cac:PaymentTerms>`;
-}
+}*/
 
 module.exports = cpegenerapago;
